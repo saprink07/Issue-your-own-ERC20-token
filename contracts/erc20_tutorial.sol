bf2b4883a161b61dd11423f6a4afaff896326b7e
saprink07 <85307972+saprink07@users.noreply.github.com>
2021-06-03 20:19:39 +0300

    Update erc20_tutorial.sol

--- contracts/erc20_tutorial.sol
+++ contracts/erc20_tutorial.sol
@@ -1,13 +1,13 @@
 pragma solidity ^0.4.24;
 
 // ----------------------------------------------------------------------------
-// '0Fucks' token contract
+// 'SURA' token contract
 //
-// Deployed to : 0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222
-// Symbol      : 0FUCKS
-// Name        : 0 Fucks Token
-// Total supply: 100000000
-// Decimals    : 18
+// Deployed to : 0x4de7e4c19ba88f3146587c528662dffcc336f00b
+// Symbol      : SR
+// Name        : SURA
+// Total supply: 100000000000
+// Decimals    : 8
 //
 // Enjoy.
 //
@@ -99,7 +99,7 @@ contract Owned {
 // ERC20 Token, with the addition of symbol, name and decimals and assisted
 // token transfers
 // ----------------------------------------------------------------------------
-contract FucksToken is ERC20Interface, Owned, SafeMath {
+SURA is ERC20Interface, Owned, SafeMath {
     string public symbol;
     string public  name;
     uint8 public decimals;
@@ -114,11 +114,11 @@ contract FucksToken is ERC20Interface, Owned, SafeMath {
     // ------------------------------------------------------------------------
     constructor() public {
         symbol = "0FUCKS";
-        name = "0 Fucks Token";
-        decimals = 18;
-        _totalSupply = 100000000000000000000000000;
-        balances[0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222] = _totalSupply;
-        emit Transfer(address(0), 0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222, _totalSupply);
+        name = "SURA";
+        decimals = 8;
+        _totalSupply = 10000000000000000000
+        balances[0x4de7e4c19ba88f3146587c528662dffcc336f00b] = _totalSupply;
+        emit Transfer(address(0), 0x4de7e4c19ba88f3146587c528662dffcc336f00b, _totalSupply);
     }
 
 
